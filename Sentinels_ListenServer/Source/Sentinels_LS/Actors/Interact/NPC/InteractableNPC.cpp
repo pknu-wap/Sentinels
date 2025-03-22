bbf45d99d753391ff2867447b3f7d633f299ba62
HyeonJun-Jung <jhj123p@gmail.com>
2025-03-22 23:53:54 +0900

    Feat : NPC, Rift, SpawnPoint (NPC, Rift) 추가
    - NPC, Rift
    - SpawnPoint Of NPC & Rift

---
+++ Sentinels_ListenServer/Source/Sentinels_LS/Actors/Interact/NPC/InteractableNPC.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Interact/NPC/InteractableNPC.h"
+
+void AInteractableNPC::Interact()
+{
+}
+
+void AInteractableNPC::ShowInteractiveUI()
+{
+}
---
+++ Sentinels_ListenServer/Source/Sentinels_LS/Actors/Interact/Rift/InteractableRift.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Interact/Rift/InteractableRift.h"
+
+void AInteractableRift::Interact()
+{
+}
+
+void AInteractableRift::ShowInteractiveUI()
+{
+}
