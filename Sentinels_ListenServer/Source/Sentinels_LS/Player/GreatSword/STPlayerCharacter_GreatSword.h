a01b44be02d52f66a814c2cc10d50672d077fd9b
HyeonJun-Jung <jhj123p@gmail.com>
2025-05-29 15:03:09 +0900

    Fix : GreatSword Character의 Passive 몽타주가 클라이언트에서 재생되지 않는 버그 수정

--- Sentinels_ListenServer/Source/Sentinels_LS/Player/GreatSword/STPlayerCharacter_GreatSword.h
+++ Sentinels_ListenServer/Source/Sentinels_LS/Player/GreatSword/STPlayerCharacter_GreatSword.h
@@ -36,6 +36,7 @@ protected:
 protected:
 	virtual void Skill_Passive_Pressed() override;
 	virtual void Skill_Passive_Pressed_Server_Implementation() override;
+	virtual void Skill_Passive_Pressed_Multicast_Implementation() override;
 
 	UFUNCTION()
 	void SetWrapTarget_Passive();
