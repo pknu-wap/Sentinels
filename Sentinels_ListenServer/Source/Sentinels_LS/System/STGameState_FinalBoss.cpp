34ac990073959ad16fd647fe5c955be34befc304
WWjj16 <lds062626@naver.com>
2026-01-29 20:28:50 +0900

    Fix : UI 수정
    - 미션 클리어 후, 아이템 선택을 키보드 -> 마우스
    - 보스 맵 미니맵 추가
    - 미션, 보스 맵에서 클라이언트 미니맵이 안나오던 버그 수정
    - 보스맵에서 팀원 아이템 정보 안나오는 버그 수정

--- Sentinels_ListenServer/Source/Sentinels_LS/System/STGameState_FinalBoss.cpp
+++ Sentinels_ListenServer/Source/Sentinels_LS/System/STGameState_FinalBoss.cpp
@@ -2,7 +2,13 @@
 
 
 #include "System/STGameState_FinalBoss.h"
-#include "STGameState_FinalBoss.h"
+#include "Net/UnrealNetwork.h"
+
+ASTGameState_FinalBoss::ASTGameState_FinalBoss() :
+	LevelTag(FSTGameplayTags::Get().Level_Boss)
+{
+	bReplicates = true;
+}
 
 void ASTGameState_FinalBoss::BeginPlay()
 {
@@ -12,6 +18,8 @@ void ASTGameState_FinalBoss::BeginPlay()
 void ASTGameState_FinalBoss::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ASTGameState_FinalBoss, LevelTag);
 }
 
 void ASTGameState_FinalBoss::OnBossCleared_Multicast_Implementation()
